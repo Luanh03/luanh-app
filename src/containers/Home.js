80b34924bd441eece72d5e8db026b4432a88f14c
Luanh03 <luanhcosta03@gmail.com>
2024-06-29 20:17:35 -0300

    Start Menu

---
+++ src/containers/Home.js
@@ -0,0 +1,24 @@
+import * as react from 'react';
+
+
+export default function Home(){
+    return(
+        <body class="main-bg">
+            <aside class="menu">
+                <section class="menu-content">
+                    <h1>Luan</h1>
+                    <nav>
+                        <ul>
+                            <li><a href="#">Presentation</a></li>
+                            <li><a href="#">Biography</a></li>
+                            <li><a href="#">Projects</a></li>
+                            <li><a href="#">Contacts</a></li>
+                        </ul>
+                    </nav>
+                </section>
+            </aside>
+
+            
+        </body>
+    );
+}
