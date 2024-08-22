6912e2159a87f5dc78a528db9c7f6f681808479b
Luanh03 <luanhcosta03@gmail.com>
2024-08-22 00:06:12 -0300

    atualizando textos

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,15 @@
 import * as react from 'react';
+import { useLayoutEffect } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 export default function Home(){
     return(
+        
         <body>
             <aside class="menu">
                 <section class="menu-content">
-                    <h1>Luan</h1>
+                    <h1>LH</h1>
                     <nav>
                         <ul>
                             <li><a href="#Presentation">Presentation</a></li>
@@ -16,20 +20,18 @@ export default function Home(){
                     </nav>
                 </section>
             </aside>
-            <section class="presentation">
-                <h1 id="Presentation">Hello, I'm Luan Henrique!</h1>
+            <section class="presentation"id="Presentation">
+                <h1 >Hello, I'm Luan Henrique!</h1>
                 <h2>FullStack Developer</h2>
-                <p>Hi, I'm Luan - Junior FullStack Developer. Here you can learn a little more about me and you will see that my relationship with technology goes far beyond lines of code, 
-                abstracting and solving problems is not just a job but it is my motivation, with this I have been making the ideas I dream come true.</p>
+                <p>I'm passionate about technology, with a hyperfocus on code development. My journey started in college with simple problems that easily took my full attention. Over time, I improved my skills as a FullStack Web Developer at the Federal Council of Engineering and Agronomy, 
+                    with challenging and innovative projects. Every day that passes my passion increases and I am ready to gain even more experience.</p>
             </section>    
             <section id="Biography" class="biography">
                 <h1>About me</h1>
-                <p>My name is Luan Henrique. Technology has always been present in my life since my childhood as I have always liked games, whether on a console or on a computer. 
-                    The codes came a little later in my life but upon first contact I was sure that this was my passion.<br />
-                    <br />I am currently an intern at the Federal Council of Engineering and Agronomy, a federal body that controls and supervises the exercise of the profession of engineers and architects in Brazil, as a Web Developer, 
-                    I work developing software, APIs, collaborating on large-scale projects, participating in the preparation and
-                    validating software requirements, monitoring projects in production,acquiring practical knowledge about the software development life cycle and carrying out maintenance so that the professionals governed by this body have quality, safety and ease according to your needs. 
-                    C#, .NET, .NET Core, ASP.NET, ASP.NET Core, SQL Server, GitLab, JavaScript and API in .NET Core have been used.
+                <p>I am a back-end developer, with extensive experience in .NET CORE, .NET Framework and ASP.NET CORE. 
+                    I am determined to solve technical system problems, improving user usability, efficient performance and clean code.<br />
+                <br/>I started my journey as a fullstack web developer, and I have always tried to constantly evolve and acquire as much knowledge as possible, facing new challenges and interacting with current technologies. 
+                After 1 year and 7 months on this path, I build efficient and innovative applications with .NET Framework, .NET CORE, ASP.NET CORE and more.
                 </p>
             </section>  
             <section class="projects">
